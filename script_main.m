d21318f9860a249b27218f7cafa55ef8b30aa0f4
SaintDevilS <mooncake91@gmail.com>
2014-05-24 11:12:45 +0300

    cleared gil's code

--- script_main.m
+++
@@ -1,9 +0,0 @@
-img = imread('LENNA.JPG');
-seg = segment_image(img);
-
-[W1,W2] = train_nn(seg(1,1),nan,nan);
-for i=1:(size(img,1)/m)
-    for j=1:(size(img,1)/m)
-        [W1,W2] =train_nn(seg(i,j,:,:),W1,W2);
-    end
-end
