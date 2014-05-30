25bb5f2d20a0d673ee93052edddabc5229016ccd
SaintDevilS <mooncake91@gmail.com>
2014-05-30 12:15:06 +0300

    added a function to display decompressed img

---
+++ display_reconstructed_img.m
@@ -0,0 +1,17 @@
+function display_reconstructed_img ( )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+    before_img = imread('LENNA.JPG');
+    load('weights'); 
+    
+    ict = ImageCompression();
+    
+    normed_img = ict.join_blocks(blocks_of_img);
+    img = ict.normed_img_to_grayscale(normed_img);
+    
+    image(img);
+    
+
+end
+
