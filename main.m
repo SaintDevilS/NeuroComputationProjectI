f66481f4df278883acdd48802d28424a0b45924f
SaintDevilS <mooncake91@gmail.com>
2014-06-03 22:11:44 +0300

    updated main according to the function

--- main.m
+++ main.m
@@ -3,7 +3,4 @@ ict = ImageCompression;
 %train_and_save_weights();
 img = imread('LENNA.JPG');
     
-normed = ict.normalize_image(img);
-blocks_of_img = ict.split_image(normed, 8, 8);
-    
-display_decompressed_img(blocks_of_img)
+display_decompressed_img(img)
